93f45c54a692730eb7f350b87a1223fdb4944eb4
YangJianbang <54735591@qq.com>
2020-09-27 09:14:41 +0800

    为了初生之土而战

---
+++ src/main/java/com/atguigu/bean/Test1.java
@@ -0,0 +1,19 @@
+package com.atguigu.bean;
+
+/**
+ * @version v1.0
+ * @ProjectName: githubidea
+ * @ClassName: Test1
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: Administrator
+ * @Date: 2020/9/26 18:30
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("今天是上分的好日子！");
+        System.out.println("把把是老八");
+        System.out.println("还不是因为技术菜！");
+        System.out.println("运气不好而已！");
+        System.out.println("为了初生之土而战！");
+    }
+}
